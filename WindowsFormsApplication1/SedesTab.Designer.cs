f0a9cadab34c9870e21c67b0cc7949c8919d4561
José Manuel <jmsanchezrico@gmail.com>
2022-11-19 10:03:15 +0100

    Project Start

---
+++ WindowsFormsApplication1/SedesTab.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class SedesTab
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.buttonExit.Location = new System.Drawing.Point(918, 351);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(89, 33);
+            this.buttonExit.TabIndex = 0;
+            this.buttonExit.Text = "SALIR";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(143, 91);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Importar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(228, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(779, 322);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // SedesTab
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1037, 396);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonExit);
+            this.Name = "SedesTab";
+            this.Text = "Sedes";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ WindowsFormsApplication1/SedesTab.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class SedesTab : Form
+    {
+        public SedesTab()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            int n_line = 0;
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "csv files (*.csv)|*.csv";
+                openFileDialog.FilterIndex = 1;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK
+                    && !openFileDialog.FileName.Equals(""))
+                {
+                    //Get the path of specified file
+                    filePath = openFileDialog.FileName;
+                    //Read the contents of the file into a stream
+                    var fileStream = openFileDialog.OpenFile();
+                    using (StreamReader reader = new StreamReader(fileStream))
+                    {
+                        string line;
+
+                        // Se lee la primera línea
+                        if (!reader.EndOfStream)
+                            line = reader.ReadLine();
+                        while (!reader.EndOfStream)
+                        {
+                            line = reader.ReadLine();
+                            string[] attributes = line.Split(';');
+                            try
+                            {
+                                if (attributes.Length == 6)
+                                {
+                                    string centro = attributes[0];
+                                    string nombre = attributes[1];
+                                    string apellido1 = attributes[2];
+                                    string apellido2 = (attributes[3].Equals("0")) ? "" : attributes[3];
+                                    string dni_nif = attributes[4];
+                                    string[] materias = attributes[5].Split(',');
+                                    Console.WriteLine("Centro:" + centro + "; Nombre:" + nombre
+                                        + ";Apellido1:" + apellido1 + "; Apelliido2:" + apellido2
+                                        + ";DNI/NIF" + dni_nif + "; Materias: " + materias[0]);
+                                }
+                                else
+                                    throw new Exception("Not enough arguments in the line " + n_line);
+                            }
+                            catch (Exception)
+                            {
+                                Console.WriteLine("Error found");
+                            }
+                            n_line++;
+                        }
+                    }
+                    MessageBox.Show("Se procesaron" + (n_line - 1) + " líneas.", "File Content at path: " + filePath, MessageBoxButtons.OK);
+                }
+            }
+        }
+    }
+}
