3901d9b1196e5af8e557a3fad6dad427c6819a96
Lucia <luciagm639@gmail.com>
2022-11-19 12:01:03 +0100

    Conexion con la base de datos
    Para conectarte tienes que instalar el nuGet de mysql

---
+++ WindowsFormsApplication1/MySqlBD.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using MySqlConnector;
+using MySqlX.XDevAPI;
+
+namespace BDLibrary
+{
+    public class MySqlBD
+    {
+        public string origen_datos;
+        public string cadenaConexion;
+        public string cadenaConexionSQLNCLI;
+
+        public MySqlBD()
+        {
+            
+                //origen_datos = pathBD;
+                //cadenaConexion = "Provider=Microsoft.ACE.OLEDB.12.0; "
+                //        + "Data Source=" + pathBD;
+            MySqlConnector.MySqlConnectionStringBuilder builder = new MySqlConnectionStringBuilder();
+            builder.Port = 3306;
+            builder.Server = "database-pevau.cobadwnzalab.eu-central-1.rds.amazonaws.com";
+            builder.UserID = "grupo10";
+            builder.Password = "6A9WnAAEaNcuYQfe";
+            builder.Database = "grupo10DB";
+            cadenaConexion = builder.ConnectionString;
+
+
+        }
+
+        public List<string> getColumnas(string tabla)
+        {
+            List<string> res = new List<string>();
+            string consultaSelect = "SELECT TOP 1 * FROM " + tabla + ";";
+            MySqlConnection conexion = new MySqlConnection(cadenaConexion);
+            MySqlCommand comando = new MySqlCommand(consultaSelect, conexion);
+
+            conexion.Open();
+            System.Data.Common.DbDataReader reader = comando.ExecuteReader();
+
+            reader.Read();
+            for (int i = 0; i < reader.FieldCount; ++i) res.Add(reader.GetName(i));
+
+            conexion.Close();
+            return res;
+        }
+
+        public List<object[]> Select(string consultaSelect)
+        {
+
+            if (consultaSelect.Length == 0) return null;
+            if (consultaSelect.ToUpper().IndexOf("SELECT") == -1) return null;
+            if (consultaSelect.ToUpper().IndexOf("DROP") > -1) return null;
+
+            List<object[]> res = new List<object[]>();
+            MySqlConnection conexion = new MySqlConnection(cadenaConexion);
+            MySqlCommand comando = new MySqlCommand(consultaSelect, conexion);
+
+            conexion.Open();
+            System.Data.Common.DbDataReader reader = comando.ExecuteReader();
+
+            while (reader.Read())
+            {
+                int numColumnas = reader.FieldCount;
+                object[] fila = new object[numColumnas];
+                for (int i = 0; i < numColumnas; ++i) fila[i] = reader[i];
+                res.Add(fila);
+            }
+
+            conexion.Close();
+
+
+            return res;
+        }
+
+        public object SelectScalar(string consultaSelect)
+        {
+
+            if (consultaSelect.Length == 0) return null;
+            if (consultaSelect.ToUpper().IndexOf("SELECT") == -1) return null;
+            if (consultaSelect.ToUpper().IndexOf("DROP") > -1) return null;
+
+
+            MySqlConnection conexion = new MySqlConnection(cadenaConexion);
+            MySqlCommand comando = new MySqlCommand(consultaSelect, conexion);
+
+            conexion.Open();
+            object res = comando.ExecuteScalar();
+
+            conexion.Close();
+
+
+            return res;
+        }
+
+        public void Update(string cadenaUpdate)
+        {
+            if (cadenaUpdate.Length == 0) return;
+
+            MySqlConnection conexion = new MySqlConnection(cadenaConexion);
+            MySqlCommand comando = new MySqlCommand(cadenaUpdate, conexion);
+
+            conexion.Open();
+            comando.ExecuteNonQuery();
+            conexion.Close();
+        }
+
+        public void Delete(string cadenaDelete)
+        {
+            if (cadenaDelete.Length == 0) return;
+
+            MySqlConnection conexion = new MySqlConnection(cadenaConexion);
+            MySqlCommand comando = new MySqlCommand(cadenaDelete, conexion);
+
+            conexion.Open();
+            comando.ExecuteNonQuery();
+            conexion.Close();
+        }
+
+        public void Insert(string cadenaInsert)
+        {
+            if (cadenaInsert.Length == 0) return;
+
+            MySqlConnection conexion = new MySqlConnection(cadenaConexion);
+
+            MySqlCommand comando = new MySqlCommand(cadenaInsert, conexion);
+
+            conexion.Open();
+            comando.ExecuteNonQuery();
+            conexion.Close();
+        }
+
+        public void EjecutaProcedimiento(string name, MySqlParameter[] param)
+        {
+            MySqlConnection conexion = new MySqlConnection(cadenaConexionSQLNCLI);
+            MySqlCommand comando = new MySqlCommand(name, conexion);
+            foreach (MySqlParameter p in param)
+            {
+                comando.Parameters.Add(p);
+            }
+            comando.CommandType = CommandType.StoredProcedure;
+            conexion.Open();
+            comando.ExecuteNonQuery();
+            conexion.Close();
+        }
+
+    }
+
+}
--- WindowsFormsApplication1/Program.cs
+++ WindowsFormsApplication1/Program.cs
@@ -17,6 +17,7 @@ namespace WindowsFormsApplication1
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new SedesTab());
+            Sede sede = new Sede();
         }
     }
 }
